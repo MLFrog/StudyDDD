7760cd005875b6d3d104e2a7ccb3fc4afbb33c94
CASY82 <repaid11010@gmail.com>
2024-04-11 21:49:41 +0900

    init : init repository

---
+++ src/test/java/com/study/codepour/Eps1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.codepour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eps1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
