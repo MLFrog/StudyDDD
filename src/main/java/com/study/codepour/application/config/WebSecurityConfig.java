cc0a9196c9297f2f352f35b592296af24e382e21
CASY82 <repaid11010@gmail.com>
2024-06-19 01:25:58 +0900

    fix : 접근제어 추가

--- src/main/java/com/study/codepour/application/config/WebSecurityConfig.java
+++ src/main/java/com/study/codepour/application/config/WebSecurityConfig.java
@@ -3,15 +3,23 @@ package com.study.codepour.application.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.study.codepour.infrastructure.jwt.JwtTokenFilter;
+
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @Configuration  
@@ -19,7 +27,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class WebSecurityConfig {
 	
-
+	private final JwtTokenFilter jwtAuthenticationFilter;
+	private final UserDetailsService userDetailsService;
 
 //    @Override
 //    public void addCorsMappings(CorsRegistry corsRegistry) {
@@ -42,10 +51,25 @@ public class WebSecurityConfig {
     	
     	http.authorizeHttpRequests(authorizeHttpRequests -> //인가 설정
                 authorizeHttpRequests
-                        .requestMatchers("/**").permitAll() 
+                		.requestMatchers("/swagger-ui/**", 
+                						 "/v3/api-docs/**", 
+                						 "/api/v1/auth/**")
+                		.permitAll()
                         .anyRequest().authenticated()
     			)
-	        .csrf(AbstractHttpConfigurer::disable);
+    		.sessionManagement(session ->
+				session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+    		)
+    		.authenticationProvider(this.authenticationProvider())
+        	.addFilterBefore(this.jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+        	.exceptionHandling(exception ->  
+            exception.authenticationEntryPoint(
+            		(request, response, authException)
+                    -> response.sendError(
+                        HttpServletResponse.SC_UNAUTHORIZED,
+                        authException.getLocalizedMessage()
+                      )))
+	      	.csrf(AbstractHttpConfigurer::disable);
         
         return http.build();  
     }  
@@ -67,18 +91,19 @@ public class WebSecurityConfig {
 //        return new BCryptPasswordEncoder();
 //    }
     
-//    @Bean
-//    public AuthenticationProvider authenticationProvider() {
-//        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-//        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
-//        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
-//        return daoAuthenticationProvider;
-//    }
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(this.userDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return daoAuthenticationProvider;
+    }
 
     @Bean  
     public WebSecurityCustomizer webSecurityCustomizer() {  
         return (web) -> web.ignoring().requestMatchers(  
                 "/swagger-ui/**",
+                "/v3/api-docs/**",
                 "/static/**"
 		);  
     }
